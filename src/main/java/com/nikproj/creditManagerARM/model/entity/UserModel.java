74c0a7619ef07e7203fce1b4800875de9bd3d310
niknikitin89 <niknikitin89@gmail.com>
2024-04-10 17:50:02 +0300

    Ограничен доступ к странице создания запросов

--- src/main/java/com/nikproj/creditManagerARM/model/entity/UserModel.java
+++ src/main/java/com/nikproj/creditManagerARM/model/entity/UserModel.java
@@ -14,6 +14,7 @@ import java.util.Arrays;
 import java.util.Collection;
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
@@ -25,6 +26,7 @@ import org.springframework.security.core.userdetails.UserDetails;
 @Entity
 @Table(name = "users")
 @Data
+@NoArgsConstructor
 public class UserModel implements UserDetails{
 
     @Id
--- src/main/java/com/nikproj/creditManagerARM/security/SecurityConfig.java
+++ src/main/java/com/nikproj/creditManagerARM/security/SecurityConfig.java
@@ -4,6 +4,7 @@
  */
 package com.nikproj.creditManagerARM.security;
 
+import com.nikproj.creditManagerARM.model.Constants;
 import com.nikproj.creditManagerARM.model.entity.UserModel;
 import com.nikproj.creditManagerARM.repository.UserDAOInterface;
 import java.util.ArrayList;
@@ -11,6 +12,7 @@ import java.util.Arrays;
 import java.util.List;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
@@ -19,6 +21,7 @@ import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
 
 /**
  *
@@ -27,6 +30,11 @@ import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 @Configuration
 public class SecurityConfig {
 
+    /**
+     * Создаем шифровальщик паролей
+     *
+     * @return
+     */
     @Bean
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
@@ -43,14 +51,48 @@ public class SecurityConfig {
 //        return new InMemoryUserDetailsManager(userList);
 //        
 //    }
-    
+    /**
+     * Служба хранения учетных записей
+     *
+     * @param repository
+     * @return
+     */
     @Bean
-    public UserDetailsService userDetailService(UserDAOInterface repository){
+    public UserDetailsService userDetailService(UserDAOInterface repository) {
         return username -> {
             UserModel user = repository.findByUsername(username);
-            if(user != null) return user;
-            
-            throw new UsernameNotFoundException("User '"+username+"' not found");
+            if (user != null) {
+                return user;
+            }
+
+            throw new UsernameNotFoundException("User '" + username + "' not found");
         };
     }
+
+    /**
+     * Настройка аутентификации при доступе по оперделенным URL
+     *
+     * @param http
+     * @return
+     * @throws Exception
+     */
+    @Bean
+    public SecurityFilterChain finterChain(HttpSecurity http) throws Exception {
+        return http
+                .authorizeHttpRequests(
+                        (request) -> {
+                            request.requestMatchers(Constants.PAGE_URL_CREATE_REQUEST)
+                                    .hasRole("USER");
+                        }
+                )//Задаем URL и описываем необходимые роли для доступа к нему
+                //В данном случае нужна роль ROLE_USER
+                .authorizeHttpRequests(
+                        (request) -> {
+                            request.requestMatchers("/**")
+                                    .permitAll();
+                        }//Всем остальным URL даем полный доступ
+                )
+                .build();
+    }
+
 }
